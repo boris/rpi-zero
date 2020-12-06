ba4924ff49fefc902596d07a469cdbd09d40436d
Boris Quiroz <boris@insert-coin.org>
2020-12-06 17:29:14 -0300

    add flask and deps

--- temp/app.py
+++ temp/app.py
@@ -2,7 +2,18 @@ import adafruit_dht
 import time
 import board
 
-dhtSensor = adafruit_dht.DHT11(board.D4)
+from flask import Flask, Response
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app, Gauge
+
+dhtSensor = adafruit_dht.DHT11(board.D17)
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+    })
+
+temp_gauge = Gauge('temp_gauge', 'Temperature in Celcius')
 
 while True:
     humidity = dhtSensor.humidity
@@ -11,3 +22,10 @@ while True:
     print(f"Temp: {temp_c}C \nHumidity: {humidity}%")
 
     time.sleep(10)
+
+@app.route('/')
+def temperature():
+    return f"Current temperature is {temp_c}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
